c5e657b5177e298c53e50a2e7ae285472327dffe
adminnono <arnaud.dujardin76@gmail.com>
2025-01-09 11:24:42 +0100

    Activer le partage de fichier

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="bg-light text-center text-lg-start mt-auto">
     <div class="text-center p-3">
-        © 2021 Copyright:
-        <a class="text-dark" href="https://openclassrooms.com/">OpenClassrooms</a>
+        © 2025 Copyright:
+        <a class="text-dark" href="https://portfolio-arnaud-dujardin.vercel.app/">Arnaud Dujardin</a>
     </div>
 </footer>
